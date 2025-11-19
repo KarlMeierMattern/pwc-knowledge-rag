680d8705e10f22be27db9bdc1dcc8ea63efff0b5
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-11-19 16:02:32 +0200

    commit

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -3,9 +3,12 @@ import { env } from "@/lib/env.mjs";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
-import "dotenv/config";
+// import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 
 const runMigrate = async () => {
+  console.log(`${process.env.DATABASE_URL} ? "✅" : "❌"`);
   if (!env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not defined");
   }
